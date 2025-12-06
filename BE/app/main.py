3e24e6dc462d7f64821b7dbafa54983b135700c9
Son Nguyen <nguyenxuansondeptrai@gmail.com>
2025-12-06 13:03:50 +0700

    ASE-41: change CORS to allow all origin & add root API

--- BE/app/main.py
+++ BE/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import health_check, auth
+
+from app.database.db_client import connect_to_mongo, close_mongo_connection
+from app.routers import auth, health_check, booking
 
 app = FastAPI()
 
@@ -22,16 +24,18 @@ app.add_middleware(
 
 app.include_router(health_check.router)
 app.include_router(auth.router)
+app.include_router(booking.router)
 
 
 def main():
     """Run the FastAPI application."""
     import uvicorn
+
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
         port=8000,
-        reload=True
+        reload=True,
     )
 
 
